3505ebe0a45816459cc688b4a4bc644a5c8a555f
chaachu <ainisyabf@yahoo.com>
2023-08-02 14:59:54 +0700

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(
+  <Canvas shadows>
+    <Experience />
+  </Canvas>
+);
