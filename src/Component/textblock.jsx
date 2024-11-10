cf8fa0eb76f3dd0627edfb816667340fbcf3104b
Andreo Friendly <102529981+andreofriendly@users.noreply.github.com>
2024-11-11 05:15:28 +0700

    check

---
+++ src/Component/textblock.jsx
@@ -0,0 +1,45 @@
+import '../App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">Sejarah Batik</h1>
+                <p id="textblock-content">
+                    Asal-usul Batik di Indonesia
+                    <br /><br />
+                    Batik pertama kali muncul di pulau Jawa pada abad ke-6 Masehi. Digunakan untuk menghiasi kain-kain keluarga kerajaan dan bangsawan, motif batik mencerminkan status sosial dan hierarki masyarakat.
+                </p>
+                <p id="textblock-content">
+                    Perkembangan dan Komodifikasi Batik
+                    <br /><br />
+                    Seiring waktu, batik diproduksi secara massal dan menjadi lebih terjangkau bagi masyarakat umum. Pengaruh kolonial Belanda pada abad ke-19 mengubah produksi batik menjadi komoditas perdagangan, menyebabkan penurunan popularitas batik tradisional.
+                </p>
+                <p id="textblock-content">
+                    Pemulihan Minat Terhadap Batik
+                    <br /><br />
+                    Pada awal abad ke-20, seniman-seniman Indonesia seperti Raden Saleh dan Affandi menghidupkan kembali minat terhadap batik tradisional. Mereka menciptakan gaya baru dengan menggabungkan elemen seni Barat dengan motif-motif batik tradisional.
+                </p>
+                <p id="textblock-content">
+                    Batik Sebagai Simbol Nasionalisme
+                    <br /><br />
+                    Selama perjuangan kemerdekaan Indonesia, batik menjadi simbol nasionalisme dan identitas bangsa. Beberapa motif batik dirancang khusus untuk mewakili semangat perjuangan dan cita-cita kemerdekaan.
+                </p>
+                <p id="textblock-content">
+                    Pengakuan UNESCO dan Popularitas Batik
+                    <br /><br />
+                    Pada tahun 2009, batik Indonesia diakui oleh UNESCO sebagai Warisan Budaya Takbenda Manusia yang membutuhkan perlindungan dan pelestarian. Pengakuan ini memberikan pengaruh positif terhadap popularitas batik di dalam negeri maupun internasional.
+                </p>
+                <p id="textblock-content">
+                    Kekayaan Budaya yang Diapresiasi
+                    <br /><br />
+                    Hingga saat ini, batik terus berkembang dan menjadi salah satu kekayaan budaya Indonesia yang paling dihargai. Setiap daerah di Indonesia memiliki gaya dan motif batik yang khas, dan batik tetap menjadi busana formal dalam acara-acara resmi.
+                    <br /><br />
+                    Batik Indonesia terus berperan dalam melestarikan warisan budaya yang kaya dan beragam di Indonesia.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default TextBlock;
